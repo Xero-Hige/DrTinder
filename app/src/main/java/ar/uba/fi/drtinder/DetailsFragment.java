2fc693a3bf93175140812e762d7c9855f7879ff6
Xero-Hige <gaston.martinez.90@gmail.com>
2016-04-02 20:50:20 -0300

    #21 User detail info based in AS template. FIXME: Rename all files to match some kind of convention

---
+++ app/src/main/java/ar/uba/fi/drtinder/DetailsFragment.java
@@ -0,0 +1,23 @@
+package ar.uba.fi.drtinder;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class DetailsFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.frament_details);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show());
+    }
+}
