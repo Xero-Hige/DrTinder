045b5a19864085ce8cb57ceec78f2157701d8f7c
chicas <debbie1new.world@gmail.com>
2016-04-07 16:44:01 -0300

    agrego libs mongoose y rocksdb

---
+++ source/Server.cpp
@@ -0,0 +1,9 @@
+#include "Server.h"
+
+Server::Server() {
+    //ctor
+}
+
+Server::~Server() {
+    //dtor
+}
---
+++ source/Server.h
@@ -0,0 +1,14 @@
+#ifndef SERVER_H
+#define SERVER_H
+
+#include "libs/mongoose-master/mongoose.h"
+
+class Server {
+    public:
+        Server();
+        virtual ~Server();
+    protected:
+    private:
+};
+
+#endif // SERVER_H
