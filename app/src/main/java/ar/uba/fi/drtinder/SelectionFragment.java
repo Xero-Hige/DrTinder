14b84ddfd23cd7d044ce7490da5f01763275485f
Xero-Hige <gaston.martinez.90@gmail.com>
2016-04-26 02:11:54 -0300

    #23 - Added reloading cards

--- app/src/main/java/ar/uba/fi/drtinder/SelectionFragment.java
+++ app/src/main/java/ar/uba/fi/drtinder/SelectionFragment.java
@@ -1,8 +1,13 @@
 package ar.uba.fi.drtinder;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.ColorStateList;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
@@ -34,27 +39,32 @@ public class SelectionFragment extends Fragment {
     public static final String EXTRA_USER_BIO = "bio";
     //TODO: Remove
     private SwipeDeck mCardStack;
+
     private boolean mShowSwipeResult = true;
+    private View mProgressView;
+
     private Queue<Map<String, String>> mUsersQueue;
     private Map<Integer, Map<String, String>> mUsersData;
+    private View fragmentView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle bundle) {
-        View view = inflater.inflate(R.layout.activity_selection, container, false);
+        fragmentView = inflater.inflate(R.layout.activity_selection, container, false);
 
-        mCardStack = (SwipeDeck) view.findViewById(R.id.swipe_deck);
+        mCardStack = (SwipeDeck) fragmentView.findViewById(R.id.swipe_deck);
         mCardStack.setHardwareAccelerationEnabled(true);
 
         requestUsersData();
 
-        setCardsAdapter(view);
+        setCardsAdapter(fragmentView);
         mCardStack.setLeftImage(R.id.card_nope);
         mCardStack.setRightImage(R.id.card_like);
+        mProgressView = fragmentView.findViewById(R.id.login_progress);
 
-        setButtons(view);
+        setButtons(fragmentView);
 
-        return view;
+        return fragmentView;
     }
 
     private void setButtons(View view) {
@@ -91,6 +101,10 @@ public class SelectionFragment extends Fragment {
 
             @Override
             public void cardsDepleted() {
+                showProgress(true);
+                UsersFetchTask mAuthTask = new UsersFetchTask(getContext());
+                mAuthTask.execute((Void) null);
+
                 Snackdebug.showMessage("No more cats", getView());
 
             }
@@ -115,6 +129,42 @@ public class SelectionFragment extends Fragment {
         });
     }
 
+    /**
+     * Shows the progress UI and hides the login form.
+     */
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+    private void showProgress(final boolean show) {
+        // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
+        // for very easy animations. If available, use these APIs to fade-in
+        // the progress spinner.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+            int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
+
+            mCardStack.setVisibility(show ? View.GONE : View.VISIBLE);
+            mCardStack.animate().setDuration(shortAnimTime).alpha(
+                    show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mCardStack.setVisibility(show ? View.GONE : View.VISIBLE);
+                }
+            });
+
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            mProgressView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+                }
+            });
+        } else {
+            // The ViewPropertyAnimator APIs are not available, so simply show
+            // and hide the relevant UI components.
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            mCardStack.setVisibility(show ? View.GONE : View.VISIBLE);
+        }
+    }
+
     private void showActualUserData() {
         Intent menuIntent = new Intent(getContext(), UserDetailsActivity.class);
         Map<String, String> data = mUsersQueue.peek();
@@ -131,65 +181,38 @@ public class SelectionFragment extends Fragment {
         mUsersQueue = new LinkedList<>();
         mUsersData = new HashMap<>();
 
-        Map<String, String> userMap = new HashMap<>();
-        userMap.put(EXTRA_USER_NAME, "Gata1");
-        userMap.put(EXTRA_USER_AGE, "21");
-        userMap.put(EXTRA_USER_IMAGE, String.valueOf(R.drawable.gato_1));
-        userMap.put(EXTRA_USER_BIO, "Una gata");
 
-        mUsersData.put(0, userMap);
-        mUsersData.put(1, userMap);
+        addUserCard(0, "Gata 1", "21", String.valueOf(R.drawable.gato_3), "Una Gata");
+        addUserCard(1, "Gata 2", "21", String.valueOf(R.drawable.gato_3), "Una Gata");
+        addUserCard(2, "Gata 3", "21", String.valueOf(R.drawable.gato_3), "Una Gata");
 
-        mUsersQueue.add(userMap);
-        mUsersQueue.add(userMap);
-
-        userMap = new HashMap<>();
-        userMap.put(EXTRA_USER_NAME, "Gata2");
-        userMap.put(EXTRA_USER_AGE, "24");
-        userMap.put(EXTRA_USER_IMAGE, String.valueOf(R.drawable.gato_2));
-        userMap.put(EXTRA_USER_BIO, "Una gata");
-
-        mUsersData.put(2, userMap);
-        mUsersQueue.add(userMap);
-
-        userMap = new HashMap<>();
-        userMap.put(EXTRA_USER_NAME, "Gata3");
-        userMap.put(EXTRA_USER_AGE, "27");
-        userMap.put(EXTRA_USER_IMAGE, String.valueOf(R.drawable.gato_3));
-        userMap.put(EXTRA_USER_BIO, "Una gata");
-
-        mUsersData.put(3, userMap);
-        mUsersQueue.add(userMap);
-
-
-        userMap = new HashMap<>();
-        userMap.put(EXTRA_USER_NAME, "Tienda");
-        userMap.put(EXTRA_USER_AGE, "35");
-        userMap.put(EXTRA_USER_IMAGE, String.valueOf(R.drawable.gato_4));
-        userMap.put(EXTRA_USER_BIO, "EN ESTE HIPERMERCADO SECTOR CARNES, BUSCO HACER LA"
+        String bio = "EN ESTE HIPERMERCADO SECTOR CARNES, BUSCO HACER LA"
                 + " DIFERENCIA. Soy de Zona sur, tengo 35 años y busco algun chico lindo e"
                 + " interesante para conocer y salir. Sin apuros, pero con buenas intenciones. Soy"
                 + " copada, simpatica y compañera. Buena gente, positiva y siempre para adelante. "
                 + "Si queres conocerme, dale like o super like (previa lectura de mi simpatica"
-                + " plaquita) =)"); //TODO: Remove
+                + " plaquita) =)";
 
-        mUsersData.put(4, userMap);
-        mUsersData.put(5, userMap);
-        mUsersQueue.add(userMap);
-        mUsersQueue.add(userMap);
+        addUserCard(3, "Tienda", "25", String.valueOf(R.drawable.gato_4), bio);
 
-        userMap = new HashMap<>();
-        userMap.put(EXTRA_USER_NAME, "Anastasia Steele");
-        userMap.put(EXTRA_USER_AGE, "21");
-        userMap.put(EXTRA_USER_IMAGE, String.valueOf(R.drawable.gato_5));
-        userMap.put(EXTRA_USER_BIO, "Joven estudiante de 21 años que cursa la carrera de Literatura"
+        bio = "Joven estudiante de 21 años que cursa la carrera de Literatura"
                 + "en la Universidad de Washington. Vivo con mi mejor amiga, Katherine Kavanagh, "
-                + "quien escribe para el periódico estudiantil de su universidad."); //TODO: Remove
+                + "quien escribe para el periódico estudiantil de su universidad.";
 
-        mUsersData.put(6, userMap);
-        mUsersQueue.add(userMap);
+        addUserCard(4, "Anastasia Steele", "21", String.valueOf(R.drawable.gato_5), bio);
 
         //TODO: remove testing data
+    }
+
+    private void addUserCard(int index, String name, String age, String image, String bio) {
+        Map<String, String> userMap = new HashMap<>();
+        userMap.put(EXTRA_USER_NAME, name);
+        userMap.put(EXTRA_USER_AGE, age);
+        userMap.put(EXTRA_USER_IMAGE, image);
+        userMap.put(EXTRA_USER_BIO, bio);
+
+        mUsersData.put(index, userMap);
+        mUsersQueue.add(userMap);
     }
 
     @Override
@@ -200,6 +223,59 @@ public class SelectionFragment extends Fragment {
     @Override
     public void onStop() {
         super.onStop();
+    }
+
+    /**
+     * Represents an asynchronous login/registration task used to authenticate
+     * the user.
+     */
+    public class UsersFetchTask extends AsyncTask<Void, Void, Boolean> {
+
+        private final Context mTaskContext;
+
+        /**
+         * Creates a new Login task
+         *
+         * @param context Calling activity context
+         */
+        UsersFetchTask(Context context) {
+            this.mTaskContext = context;
+        }
+
+        /**
+         * @param params params
+         * @return Task successful
+         */
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            try {
+                // Simulate network access.
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                return false;
+            }
+            return true;
+        }
+
+        @Override
+        protected void onPostExecute(final Boolean success) {
+            showProgress(false);
+
+            if (success) {
+                requestUsersData();
+                setCardsAdapter(fragmentView);
+                showProgress(false);
+            } else {
+                Snackdebug.showMessage("Really, no more cats", getView());
+            }
+        }
+
+        @Override
+        protected void onCancelled() {
+            requestUsersData();
+            setCardsAdapter(fragmentView);
+            showProgress(false);
+        }
     }
 
     /**
