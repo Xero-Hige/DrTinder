4cd97bfde0cd2cc73bc2759de93484f2753da4e8
Xero-Hige <gaston.martinez.90@gmail.com>
2016-06-08 16:18:08 -0300

    #18 - Added mock Pick avatar image at profile

--- app/src/main/java/ar/uba/fi/drtinder/UserProfile.java
+++ app/src/main/java/ar/uba/fi/drtinder/UserProfile.java
@@ -1,18 +1,21 @@
 package ar.uba.fi.drtinder;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.widget.ImageView;
 
 public class UserProfile extends AppCompatActivity {
 
+    private static final int PICK_IMAGE = 100;
     public static String USER_EXTRA_USERNAME = "name";
     public static String PROFILE_EXTRA_ACTION = "action";
-
     public static String PROFILE_ACTION_CREATE = "create";
     public static String PROFILE_ACTION_UPDATE = "Update Profile";
+    private ImageView profilePic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,8 +28,24 @@ public class UserProfile extends AppCompatActivity {
 
         toolbar.setTitle(activityIntent.getStringExtra(PROFILE_ACTION_UPDATE));
 
-        ImageView profilePic = (ImageView) findViewById(R.id.userAvatar);
+        profilePic = (ImageView) findViewById(R.id.userAvatar);
         ImageResourcesHandler.fillImageResource(activityIntent.getStringExtra(USER_EXTRA_USERNAME), ImageResourcesHandler.RES_USER_IMG, profilePic, this);
 
+        assert profilePic != null; //DEBUG Assert
+
+        profilePic.setOnClickListener(v -> {
+            Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+            startActivityForResult(gallery, PICK_IMAGE);
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == PICK_IMAGE) {
+            Uri imageUri = data.getData();
+            profilePic.setImageURI(imageUri);
+        }
     }
 }
