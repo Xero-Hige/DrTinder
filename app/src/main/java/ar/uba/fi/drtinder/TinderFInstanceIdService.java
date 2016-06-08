466844c152288846a177e09d20eaeef4b07d96f5
Xero-Hige <gaston.martinez.90@gmail.com>
2016-06-08 18:54:19 -0300

    #61 - Added base FCM services. Based on: https://github.com/firebase/friendlychat

---
+++ app/src/main/java/ar/uba/fi/drtinder/TinderFInstanceIdService.java
@@ -0,0 +1,45 @@
+package ar.uba.fi.drtinder;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+/**
+ * ${FILE}
+ * <p>
+ * Copyright 2016 Gaston Martinez Gaston.martinez.90@gmail.com
+ * <p>
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses
+ */
+public class TinderFInstanceIdService extends FirebaseInstanceIdService {
+
+    private static final String TAG = "MyFirebaseIIDService";
+    private static final String FRIENDLY_ENGAGE_TOPIC = "friendly_engage";
+
+    /**
+     * The Application's current Instance ID token is no longer valid
+     * and thus a new one must be requested.
+     */
+    @Override
+    public void onTokenRefresh() {
+        // If you need to handle the generation of a token, initially or
+        // after a refresh this is where you should do that.
+        String token = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "FCM Token: " + token);
+
+        // Once a token is generated, we subscribe to topic.
+        FirebaseMessaging.getInstance()
+                .subscribeToTopic(FRIENDLY_ENGAGE_TOPIC);
+    }
+}
---
+++ app/src/main/java/ar/uba/fi/drtinder/TinderFMService.java
@@ -0,0 +1,36 @@
+package ar.uba.fi.drtinder;
+
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+/**
+ * ${FILE}
+ * <p>
+ * Copyright 2016 Gaston Martinez Gaston.martinez.90@gmail.com
+ * <p>
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses
+ */
+public class TinderFMService extends FirebaseMessagingService {
+
+    private static final String TAG = "MyFMService";
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        // Handle data payload of FCM messages.
+        Log.d(TAG, "FCM Message Id: " + remoteMessage.getMessageId());
+        Log.d(TAG, "FCM Notification Message: " +
+                remoteMessage.getNotification());
+        Log.d(TAG, "FCM Data Message: " + remoteMessage.getData());
+    }
+}
