bd654c5d824a62f96d2c54148705478f70622530
Xero-Hige <gaston.martinez.90@gmail.com>
2016-06-20 18:06:16 -0300

    Changed language to english (to match doc language)

--- doc/TechnicalDoc/source/conf.py
+++ doc/TechnicalDoc/source/conf.py
@@ -73,7 +73,7 @@ release = u'1.0.0'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = 'es'
+language = 'en'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
@@ -198,7 +198,7 @@ html_show_copyright = True
 # Sphinx supports the following languages:
 #   'da', 'de', a, 'es', 'fi', 'fr', 'hu', 'it', 'ja'
 #   'nl', 'no', 'pt', 'ro', 'ru', 'sv', 'tr'
-html_search_language = 'es'
+html_search_language = 'en'
 
 # A dictionary with options for the search language support, empty by default.
 # Now only 'ja' uses this config value
