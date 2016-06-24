2500f92086a5e8e2df64ffb0a2c2f7c234c7eddc
tobiasbianchi <tobias.bianchi93@gmail.com>
2016-06-24 17:26:58 -0300

    Set vars: port,shared and loc conf from argv,argc

--- source/Servidor/tests/main.cpp
+++ source/Servidor/tests/main.cpp
@@ -7,20 +7,31 @@
 
 #include "interestTest.h"
 #include "parserTest.h"
-#include "sharedServerTest.h"
+/*#include "sharedServerTest.h"
 #include "tokenizerTest.h"
 #include "userMatcherTest.h"
-#include "messageHandlerTest.h"
+#include "messageHandlerTest.h"*/
 #include "gtest/gtest.h"
 #include "../../libs/loger/easylogging++.h"
+#include "../ServerUtils.h"
+
 INITIALIZE_EASYLOGGINGPP
 
+
 int main(int argc, char **argv) {
+  string port, shared, logCnfg;
+  configure(argc,argv,port,shared,logCnfg);
+  el::Configurations conf(logCnfg);
+  el::Loggers::reconfigureAllLoggers(conf);
+  el::Loggers::addFlag(el::LoggingFlag::ColoredTerminalOutput);
+  LOGG(INFO) << "Port: " << port;
+  LOGG(INFO) << "Shared: " << shared;
+  LOGG(INFO) << "Loger configuration: " << logCnfg;
   LOGG(DEBUG) << "Corro test";
   ::testing::InitGoogleTest(&argc, argv);
-  RestClient::init();
+  //RestClient::init();
   int result = RUN_ALL_TESTS();
-  RestClient::disable();
+  //RestClient::disable();
   LOGG(DEBUG) << "End test";
   return result;
 }
